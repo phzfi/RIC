983026080034416cc6cf7d932dc33a6d9b9285a3
kristian <kristian.mort@phz.fi>
2015-10-28 19:02:22 +0200

    [TASK-#4] Preparing for graceful HTTP server and tests are in progress

--- server/main.go
+++ server/main.go
@@ -1,25 +1,40 @@
 package main
 
 import (
+	"gopkg.in/tylerb/graceful.v1"
+	"log"
 	"net/http"
+	"time"
 )
 
-type MyServer struct {
+type MyHandler struct {
 }
 
-func (*MyServer) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
+func (*MyHandler) ServeHTTP(writer http.ResponseWriter, request *http.Request) {
 	result := "Hello world!"
 	omg := ([]byte)(result)
-	i, err := writer.Write(omg)
+	_, err := writer.Write(omg)
+	if err != nil {
+		log.Println(err)
+	}
 }
 
-func main() {
+func NewServer() {
+	handler := MyHandler{}
 	server := &http.Server{
-		Addr:           ":8000",
-		Handler:        null,
+		Addr:           ":8005",
+		Handler:        &handler,
 		ReadTimeout:    5 * time.Second,
 		WriteTimeout:   5 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
-	server.ListenAndServe()
+	return server
+}
+
+func main() {
+	server := NewServer()
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatal(err)
+	}
 }
