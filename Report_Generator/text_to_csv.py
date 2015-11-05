59364407fde5f488785a8cc3d49058cc058a2772
Lauri Telama <lauri.telama@aalto.fi>
2015-11-05 18:24:10 +0200

    text_to_csv is done but not tested
    test_to_csv loops the textLogs directory, goes throught the text logs
    and makes csv files out of the which it saves to csvFiles and then
    removes them.

--- Report_Generator/text_to_csv.py
+++ Report_Generator/text_to_csv.py
@@ -4,20 +4,29 @@ Created on 5 Nov 2015
 @author: Lauri, Joona
 '''
 import csv
+import sys
+import os
 
 def main():
-    filename = "testtext"
-    data = read_text(filename+".txt")
-    save_to_csv(filename+".csv", data)
+    
+    text_directory = "textLogs"+os.sep
+    csv_directory = "csvFiles"+os.sep
+    for filename in os.listdir(text_directory):
+            data = read_text(text_directory+filename)
+            save_to_csv(csv_directory+filename[:-4]+".csv", data)
+            os.remove(text_directory+filename)
+    
+
+    
     
 #The following line is an example line from the text Document
 #HTTP/1.1 200   0.04 secs:  653743 bytes ==> GET  /images/383504_9b66b4a1f2_o.jpg
 #Returns a list which contains lists which contain round trip time taken,
 #bytes transferred and get Path
-def read_text(filename):
+def read_text(path):
     values = []
     try:      
-        textfile = open(filename, "r")
+        textfile = open(path, "r")
         for line in textfile:
             try:
                 splitted = line.split("  ")
@@ -32,19 +41,22 @@ def read_text(filename):
                 print(line)
         textfile.close()
     except OSError:
-        print("Problem reading text file: ", filename)
+        print("Problem reading text file: ", path)
     return values
     
-def save_to_csv(filename, data):
+#saves the data obtained from read_text into a csv file
+def save_to_csv(path, data):
     try:
-        file = open(filename, 'w')
-        writer = csv.writer(file, dialect = "excel")
+        file = open(path, 'w')
+        writer = csv.writer(file, dialect = "excel", lineterminator='\n')
         for line in data:
             writer.writerow(line)
             
         file.close()
     except OSError:
-        print("Problem saving to csv file")
-    
-    
+        print("OSError while saving to csv file from file:",path)
+        sys.exit
+    except:
+        print("Other error occured while saving to csv:", path)
+        sys.exit()
 main()
