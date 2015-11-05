628a64a7c6a0e03bb0ad5bcadc51962c60791e00
Lauri Telama <lauri.telama@aalto.fi>
2015-11-05 16:18:52 +0200

    finished read_text method (not tested)
    The read_test method is done, but needs testing still.

--- Report_Generator/text_to_csv.py
+++ Report_Generator/text_to_csv.py
@@ -7,11 +7,27 @@ Created on 5 Nov 2015
 def main():
     "lol"
     
-
-#Returns list of 
+#The following line is an example line from the text Document
+#HTTP/1.1 200   0.04 secs:  653743 bytes ==> GET  /images/383504_9b66b4a1f2_o.jpg
+#Returns a list which contains lists which contain round trip time taken,
+#bytes transferred and get Path
 def read_text(filename):
-    "something"
-    
+    list = []
+    try:
+        
+        textfile = open(filename, "r")
+        for line in textfile:
+            try:
+                splitted = line.split("  ")
+                time = float(splitted[1].split(" ")[0])
+                bytes = int(splitted[2].split(" ")[0])
+                path = splitted[3]
+                list.append([time, bytes, path])
+            except ValueError:
+                print(line)
+    except OSError:
+        print("Problem reading text file: ", filename)
+    return list
     
 def save_to_csv(filename):
     "something"
