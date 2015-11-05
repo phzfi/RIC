764617562a0bfb5fc2901104c6c3dad0ef6403d0
Lauri Telama <lauri.telama@aalto.fi>
2015-11-05 15:44:02 +0200

    Added new project Report Generator
    Report generator reads text files from siege and saves them to a csv
    format

---
+++ Report_Generator/text_to_csv.py
@@ -0,0 +1,19 @@
+'''
+Created on 5 Nov 2015
+
+@author: Lauri, Joona
+'''
+
+def main():
+    "lol"
+    
+
+#Returns list of 
+def read_text(filename):
+    "something"
+    
+    
+def save_to_csv(filename):
+    "something"
+    
+main()
