9caff8dbf5a7443cb34689bc6e75566946dde1a1
Miika Piiroinen <miika.piiroinen@aalto.fi>
2015-11-10 21:29:30 +0200

    TASK-#1 is now merged with TASK-#4

--- server/images/resize.go
+++ server/images/resize.go
@@ -4,18 +4,12 @@ import (
 	"github.com/joonazan/imagick/imagick"
 )
 
-func (img ImageBlob) Resized(w, h uint) (resized ImageBlob, err error) {
-	imagick.Initialize()
-	defer imagick.Terminate()
 
-	mw := imagick.NewMagickWand()
-	mw.ReadImageBlob(img)
-	err = mw.ResizeImage(w, h, imagick.FILTER_LANCZOS, 1)
-	if err != nil {
-		return
-	}
+func (img Image) Resized(w, h uint) (resized Image, err error) {
 
-	resized = mw.GetImageBlob()
-	return
+        resized = img.Clone()
 
+	err = resized.ResizeImage(w, h, imagick.FILTER_LANCZOS, 1)
+
+        return
 }
