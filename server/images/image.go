febfc9108a203c6eac365c700b49253cda3c37ba
Miika Piiroinen <miika.piiroinen@aalto.fi>
2015-11-10 21:14:46 +0200

    Fixing after merge

--- server/images/image.go
+++ server/images/image.go
@@ -9,6 +9,9 @@ func init () {
     imagick.Initialize()
 }
 
+// Imageblob is just an image file dumped, byte by byte to an byte array.
+type ImageBlob []byte
+
 //Image is an uncompressed image that must be convertd to blob before serving to a client.
 type Image struct {
     *imagick.MagickWand
