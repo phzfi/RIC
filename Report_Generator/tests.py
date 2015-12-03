c30b622589d052634a2c62a9ef9d0a5275cf8651
Lauri Telama <telamal1@aalto.fi>
2015-12-03 17:36:42 +0200

    Cleaned up the code
    TODO: fix error handling

--- Report_Generator/tests.py
+++ Report_Generator/tests.py
@@ -1,65 +1,69 @@
-'''
+"""
 Created on 5 Nov 2015
 
 @author: Lauri, Joona
-'''
+
+"""
 import unittest
 import text_to_csv
 
-#TODO: About everything.
 class TestGenerator(unittest.TestCase):
 
 
     def test_read_text_correct(self):
-        
+
         data = text_to_csv.read_text('testfiles/testtextcorrect.txt')
-        
+
         self.assertEqual(len(data), 10, 'data was not correct length')
 
         for i in range(9):
-            self.assertEqual(data[i], [float(i), i * 10, '/images/14857596084_b0105b8e88_o.jpg', 'HTTP/1.1 200'],
-                             'wrong line value ' + str(i))
-    
+            self.assertEqual(data[i], [float(i), i * 10, 
+                        '/images/14857596084_b0105b8e88_o.jpg', 'HTTP/1.1 200'],
+                        'wrong line value ' + str(i))
+
     def test_read_text_error(self):
-        
+
         data = text_to_csv.read_text('testfiles/testtexterror.txt')
-        
+
         self.assertEqual(len(data), 10, 'data was not correct length')
 
         for i in range(9):
-            self.assertEqual(data[i], [float(i), i * 10, '/images/14857596084_b0105b8e88_o.jpg', 'HTTP/1.1 200'],
-                             'wrong line value ' + str(i))
+            self.assertEqual(data[i], [float(i), i * 10,
+                            '/images/14857596084_b0105b8e88_o.jpg', 'HTTP/1.1 200'],
+                            'wrong line value ' + str(i))
 
     def test_read_text_empty(self):
 
         data = text_to_csv.read_text('testfiles/testtextempty.txt')
-        
+
         self.assertEqual(len(data), 0, 'data was not empty')
-    
 
 
     def test_read_log(self):
 
         data = text_to_csv.read_log('testfiles/testlog.log')
-        
+
         self.assertEqual(len(data), 3, 'data was not correct length')
 
-        self.assertEqual(data['2015-10-29 16.34.29'], ['2015-10-29 16:34:29', '400', '24.50', '833',
-                                                       '0.54', '16.33', '34.00', '8.88', '400', '0'],
-                                                       'wrong data value 1')
-        self.assertEqual(data['2015-10-29 16.35.45'], ['2015-10-29 16:35:45', '400', '25.39', '833', 
-                                                       '0.60', '15.75', '32.81', '9.40', '400', '0'],
-                                                       'wrong data value 2')
-        self.assertEqual(data['2015-10-29 16.53.23'], ['2015-10-29 16:53:23', '400', '23.98', '833',
-                                                       '0.45', '16.68', '34.74', '7.55', '400', '0'], 
-                                                       'wrong data value 3')
-    
+        self.assertEqual(data['2015-10-29 16.34.29'],
+                         ['2015-10-29 16:34:29', '400', '24.50', '833',
+                          '0.54', '16.33', '34.00', '8.88', '400', '0'],
+                          'wrong data value 1')
+        self.assertEqual(data['2015-10-29 16.35.45'],
+                        ['2015-10-29 16:35:45', '400', '25.39', '833', 
+                         '0.60', '15.75', '32.81', '9.40', '400', '0'],
+                         'wrong data value 2')
+        self.assertEqual(data['2015-10-29 16.53.23'], 
+                        ['2015-10-29 16:53:23', '400', '23.98', '833',
+                         '0.45', '16.68', '34.74', '7.55', '400', '0'], 
+                         'wrong data value 3')
+
     def test_read_log_empty(self):
 
         data = text_to_csv.read_log('testfiles/testlogempty.log')
-        
+
         self.assertEqual(len(data), 0, 'data was not correct length')
-    
+
 
 if __name__ == "__main__":
     #import sys;sys.argv = ['', 'Test.testName']
--- Report_Generator/text_to_csv.py
+++ Report_Generator/text_to_csv.py
@@ -2,6 +2,7 @@
 Created on 5 Nov 2015
 
 @author: Lauri, Joona
+
 """
 import csv
 import sys
@@ -12,8 +13,6 @@ import codecs
 """
 TODO: Change directories and filenames after we have chosen the directory
 structure and the naming policy for the log files.
-TODO: Proper tests!
-
 
 The test text logs are saved in the text_directory
 The created csv files are saved into the csv directory
@@ -23,6 +22,7 @@ And checks that the test is found in the logs.txt file.
 If it is, it will be converted to a csv format and saved
 in the csv_directory.
 All test text logs are deleted. The log.txt file is untouched.
+
 """
 
 logging.basicConfig(filename='Report_Generator_error.log',
@@ -33,23 +33,24 @@ def main():
     text_directory = 'textLogs' 
     csv_directory = 'csvFiles'
     logs = read_log('siege.log')
-    
+
     if len(logs) == 0:
         logging.critical('siege.log is empty.')
         sys.exit(1)
-    
+
     try:
         text_dir_list = os.listdir(text_directory)
     except Exception as e:
         logging.critical(str(e) + ' Critical error while listing text directory')
         sys.exit(1)
-    
+
     """
     Goes through all files in text directory and attempts to convert them to a csv file. 
     The text file is removed in the process if conversion is successful or if text file
     does not appear in the logs file.
+
     """
-        
+
     for filename in text_dir_list:
         basename, _ = os.path.splitext(filename)
         if basename in logs:    
@@ -63,27 +64,29 @@ def main():
 
 def read_log(path):
     """
-    Reads the log files and returns a map of the log data where the timestamp is a key.
+    Reads the log files and returns a map of the log data where the 
+    timestamp is a key.
     Two top lines of an example log file is shown below:
 
     Date & Time,  Trans,  Elap Time,  Data Trans,  Resp Time,  Trans Rate,  Throughput,  Concurrent,    OKAY,   Failed
     2015-10-29 16:34:29,    400,      24.50,         833,       0.54,       16.33,       34.00,        8.88,     400,       0
+
     """
 
     timestamp_map = {}    
     try:
         with codecs.open(path, 'r', 'utf-8') as log:
             for line in log:
-                splitted = line.split(',')
-                                    
+                splitted = line.split(',')                    
                 """
                 Windows doesn't allow ':' in filenames, so 
                 they are replaced with '.'.
+
                 """
                 timestamp = splitted[0].replace(':','.').strip()
                 if timestamp != 'Date & Time':
                     timestamp_map[timestamp] = [x.strip() for x in splitted] 
-           
+
     except OSError as o:
         logging.critical(str(o) + ' while reading log at: ' + path)
         sys.exit(1)
@@ -102,6 +105,7 @@ def read_text(path):
 
     Returns a list which contains lists which contain round trip time taken,
     bytes transferred, GET path and server response.
+
     """
     values = []
     try:      
@@ -111,7 +115,8 @@ def read_text(path):
                     server_response, transfer_data, image_path = line.split('  ')
                     transfer_data = transfer_data.strip()
                     roundtrip_time, _, byte_data, _, _, _ = transfer_data.split(' ')
-                    values.append([float(roundtrip_time), int(byte_data), image_path.strip(), server_response.strip()])
+                    values.append([float(roundtrip_time), int(byte_data), 
+                                   image_path.strip(), server_response.strip()])
                 except ValueError as v:
                     logging.error(str(v) + ' in the following line: ' + line)
 
@@ -121,16 +126,17 @@ def read_text(path):
         logging.critical(str(e) + ' while reading text at: ' + path)
         sys.exit(1)
     return values
-    
+
 
 def save_to_csv(path, data, summary_data):
     """ Saves the data obtained from read_text and read_log into a csv file. """
     try:
         with codecs.open(path, 'w', 'utf-8') as output:
             writer = csv.writer(output, dialect = 'excel', lineterminator='\n')
-                
-            writer.writerow(['Date & Time','Trans','Elap Time', 'Data Trans', 'Resp Time',
-                       'Trans Rate', 'Throughput', 'Concurrent', 'OKAY', 'Failed'])
+
+            writer.writerow(['Date & Time','Trans','Elap Time', 'Data Trans',
+                             'Resp Time','Trans Rate', 'Throughput',
+                             'Concurrent', 'OKAY', 'Failed'])
             writer.writerow(summary_data)
             writer.writerow([''])
             writer.writerow([''])
@@ -140,9 +146,10 @@ def save_to_csv(path, data, summary_data):
             return
     except OSError as o:
         logging.critical(str(o) + ' while saving to csv file from file: ' + path)
-        
+
     except Exception as e:
         logging.critical(str(e) + ' exception while saving to csv: ' + path)
     sys.exit(1)
-main()
 
+if __name__ == '__main__':
+    main()
