2c976f4a2fdb6695dc2d392cda8e1c08e4d65132
Miika Piiroinen <miika.piiroinen@aalto.fi>
2015-12-18 23:09:30 +0200

    Fixed .gitignore. Removed the lay-siege script - run-test.sh should be sufficient. Removed some unneccessary files. config file is now set to ./.siegerc in the run-test.sh script

--- Report_Generator/run-test.sh
+++ Report_Generator/run-test.sh
@@ -3,7 +3,7 @@ OUTNAME=./testResults/$(date +%Y-%m-%d_%H-%M-%S).csv
 TMP=./temp/temp$(date +%Y-%m-%d_%H-%M-%S).txt
 
 # Siege
-siege --verbose --concurrent=2 --internet --delay=3 --time=40s --log=$OUTNAME --file=urls.txt |
+siege -R .siegerc --verbose --concurrent=2 --internet --delay=3 --time=40s --log=$OUTNAME --file=urls.txt |
 	 sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" > $TMP 
 
 cat $TMP >> $OUTNAME
