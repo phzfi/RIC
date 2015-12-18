3d2e0f403a681e8a6d16aef73f5605dc2c3d1875
Lauri Keronen <lauri.keronen@aalto.fi>
2015-12-18 14:48:54 +0200

    Small bug fixes.

--- research/clientsideDPIdetection/test.js
+++ research/clientsideDPIdetection/test.js
@@ -1,11 +1,31 @@
 
 window.onload = function() {
+  setImdivSize();
+  setScreenInfo();
+}
+
+window.onresize = function() {
+  setImdivSize();
+  setScreenInfo();
+  document.getElementById('I').src = "";
+}
+
+// get image
+function buttonf(b) {
+  var imgW = document.getElementById('imdiv').clientWidth;
+  var imgH = Math.round(imgW / 16 * 9);
+  var url = 'http://ric.phz.fi:8005/'+b.innerHTML+'?width='+imgW+'&height='+imgH;
+  document.getElementById('I').src = url;
+}
+
+function setImdivSize() {
   // use aspect ratio of 16:9 to set image height
-  imgW = document.getElementById('imdiv').clientWidth;
-  imgH = Math.round(imgW / 16 * 9);
+  var imgW = document.getElementById('imdiv').clientWidth;
+  var imgH = Math.round(imgW / 16 * 9);
   document.getElementById('imdiv').style.height = imgH + 'px';
+}
 
-
+function setScreenInfo() {
   // show screen information
   var w = screen.width;
   var h = screen.height;
@@ -13,13 +33,9 @@ window.onload = function() {
   var text = 'Your logical screen size is: ' + w + 'x'+ h + ' (' + dpr + ')<br>';
   text += 'Maximum image size for you: ' + Math.round(w * dpr) + 'x';
   text += Math.round(h * dpr) + '<br>';
-  text += 'imgdiv size: ' + imgW + 'x' + imgH;
+  //text += 'imgdiv size: ' + imgW + 'x' + imgH;
+  text += '<br>' + 'imgdiv clientWidth: ' + document.getElementById('imdiv').clientWidth;
+  text += '<br>' + 'imgdiv offsetWidth: ' + document.getElementById('imdiv').offsetWidth;
+  text += '<br>' + 'imgdiv scrollWidth: ' + document.getElementById('imdiv').scrollWidth;
   document.getElementById('infodiv').innerHTML = text;
-}
-
-
-// get image
-function buttonf(b) {
-  var url = 'http://ric.phz.fi:8005/'+b.innerHTML+'?width='+imgW+'&height='+imgH;
-  document.getElementById('I').src = url;
 }
