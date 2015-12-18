5ec77e1457ba1949c4a43dbe67fa5c847211a718
Miika Piiroinen <miika.piiroinen@aalto.fi>
2015-12-18 21:14:49 +0200

    Tests for checking that right images and image formats are returned by the server

--- server/server_test.go
+++ server/server_test.go
@@ -10,88 +10,162 @@ import (
 	"net/http"
 	"path/filepath"
 	"testing"
+	"fmt"
 	"time"
 )
 
 // This is an utility function to launch a graceful server.
 // This is intended to be run as a goroutine as it takes an
 // error channel as the first parameter.
-func startServer(errors chan<- error, server *graceful.Server) {
-	errors <- server.ListenAndServe()
+func startServer(srverr chan<- error, server *graceful.Server) {
+	srverr <- server.ListenAndServe()
 }
+
+
+// Stop server and block until stopped
+func stopServer(server *graceful.Server) {
+	server.Stop(0)
+	<- server.StopChan()
+}
+
 
 // Test that the web server return "Hello world" and does not
 // raise any exceptions or errors. This also starts and stops
-// a web server instance for the duration of the test.
-func TestHello(test *testing.T) {
+// a web server instance for the duration of the t.
+func TestHello(t *testing.T) {
+	
+	// Start the server
 	server, _ := NewServer(500000)
-	errors := make(chan error)
-
-	go startServer(errors, server)
-	defer server.Stop(3 * time.Second)
+	srverr := make(chan error)
+	go startServer(srverr, server)
+	defer stopServer(server)
+	time.Sleep(100 * time.Millisecond)
 
 	resp, err := http.Post("http://localhost:8005", "text/plain", nil)
 	if err != nil {
-		test.Fatal(err)
+		t.Fatal(err)
 	}
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		test.Fatal(err)
+		t.Fatal(err)
 	}
 
 	expected := ([]byte)("Hello world!")
 
-	ok := bytes.Equal(expected, body)
-	if !ok {
-		test.Fatal("Server did not greet us properly!")
+	if !bytes.Equal(expected, body) {
+		t.Fatal("Server did not greet us properly!")
 	}
-
-	if len(errors) > 0 {
-		err, ok := <-errors
-		if !ok {
-			// TODO: You should do this properly
-			test.Fatal("There was an error, but we missed it (too soon or too late")
-		}
-		if err != nil {
-			test.Fatal(err)
-		}
+	
+	// Yes we do this properly
+	select {
+		case err := <- srverr:
+			t.Fatal(err)
+		default:
 	}
 }
 
-// Test that the web server returns requested image at right size
-func TestGetImageFromServer(t *testing.T) {
-	const tolerance = 0.002
-	server, _ := NewServer(500000)
-	errors := make(chan error)
+const TOLERANCE = 0.0005
 
-	go startServer(errors, server)
-	defer server.Stop(3 * time.Second)
-
-	resp, err := http.Get("http://localhost:8005/testimages/server/toresize.jpg?width=100&height=100")
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer resp.Body.Close()
-
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	distortion, err := GetDistortion(body, "resized.jpg")
+// Test that the web server returns requested JPG image at right size
+func TestGetJPGFromServer(t *testing.T) {
+	err := testGetImageFromServer("toget.jpg", "getref.jpg", TOLERANCE)
 	if err != nil {
 		t.Fatal(err)
 		return
 	}
+}
 
-	if distortion > tolerance {
-		t.Fatal("Bad image returned. Distortion:", distortion, "Tolerance:", tolerance)
+// Test that the web server returns requested JPG image at right size
+func TestGetPNGFromServer(t *testing.T) {
+	err := testGetImageFromServer("toget.png", "getref.png", TOLERANCE)
+	if err != nil {
+		t.Fatal(err)
+		return
 	}
 }
 
-func GetDistortion(imageblob images.ImageBlob, filename_cmp string) (distortion float64, err error) {
+// Test that the web server returns requested JPG image at right size
+func TestGetGIFFromServer(t *testing.T) {
+	err := testGetImageFromServer("toget.gif", "getref.gif", TOLERANCE)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+}
+
+// Test that the web server returns requested JPG image at right size
+func TestGetTIFFFromServer(t *testing.T) {
+	err := testGetImageFromServer("toget.tiff", "getref.tiff", TOLERANCE)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+}
+
+// Test that the web server returns requested JPG image at right size
+func TestGetWEBPFromServer(t *testing.T) {
+	err := testGetImageFromServer("toget.webp", "getref.webp", TOLERANCE)
+	if err != nil {
+		t.Fatal(err)
+		return
+	}
+}
+
+func testGetImageFromServer(getname string, refname string, tolerance float64) (err error) {
+
+	// Start the server
+	server, _ := NewServer(500000)
+	srverr := make(chan error)
+	go startServer(srverr, server)
+	defer stopServer(server)
+	time.Sleep(100 * time.Millisecond)
+
+	// Get and read requested image (blob) of size 100x100 from the server
+	resp, err := http.Get("http://localhost:8005/testimages/server/" + getname + "?width=100&height=100")
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return
+	}
+	
+	// Save retrieved image to testresults
+
+	mw := imagick.NewMagickWand()
+	//defer mw.Destroy()
+	err = mw.ReadImageBlob(body)
+	if err != nil {
+		return
+	}
+	err = mw.WriteImage(filepath.FromSlash("testresults/server/" + getname))
+	if err != nil {
+		return
+	}
+	
+	// Get distortion compared to refrence image and check it is inside tolerance
+	distortion, err := getDistortion(body, refname)
+	if err != nil {
+		return
+	}
+	if distortion > tolerance {
+		return errors.New(fmt.Sprintf("Bad image returned. Distortion: %v, Tolerance: %v", distortion, tolerance))
+	}
+
+	// Check for server errors
+	select {
+		case err = <- srverr:
+			return
+		default:
+	}
+	
+	return
+}
+
+func getDistortion(imageblob images.ImageBlob, filename_cmp string) (distortion float64, err error) {
 	const image_folder = "testimages/server/"
 	imagick.Initialize()
 	defer imagick.Terminate()
@@ -108,7 +182,10 @@ func GetDistortion(imageblob images.ImageBlob, filename_cmp string) (distortion 
 	mw := imagick.NewMagickWand()
 	defer mw.Destroy()
 
-	mw.ReadImageBlob(imageblob)
+	err = mw.ReadImageBlob(imageblob)
+	if err != nil {
+		return
+	}
 
 	trash, distortion := mw.CompareImages(mw_cmp, imagick.METRIC_MEAN_SQUARED_ERROR)
 	trash.Destroy()
