b120831f31385c186864ba0041b741efbc923f06
Miika Piiroinen <miika.piiroinen@aalto.fi>
2015-12-21 21:15:37 +0200

    Fix scripts more

--- report_generator/run_siege_test.sh
+++ report_generator/run_siege_test.sh
@@ -12,8 +12,8 @@ TIME="40s"
 # Siege
 siege -R $SIEGE_CONF --verbose --concurrent=$CONCURRENT --internet --delay=$DELAY --time=$TIME --log=$RAW_FILE --file=$URLS_FILE |
 	 sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" > $TMP
-
-cat $TMP > $OUT_FILE
+cat $TMP >> $RAW_FILE
+rm $TMP
 
 # Formatter
 python csv_formatter.py $RAW_FILE $OUT_FILE
