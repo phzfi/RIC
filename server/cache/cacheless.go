c6705edd3a4977a5f2930a30833f856049575268
Miika Piiroinen <miika.piiroinen@aalto.fi>
2015-12-21 03:29:00 +0200

    Added new field "original" to ImageInfo. ImageInfo with original=true and width=height=0 is indicating original sized image. Added GetOriginal -method to ImageCache types. Server now serves original sized image whenever width or height parameter is left out.

--- server/cache/cacheless.go
+++ server/cache/cacheless.go
@@ -15,25 +15,73 @@ type Cacheless struct {
 }
 
 // Get image whose dimensions are known.
-func (self *Cacheless) GetImage(filename string, width, height uint) (blob images.ImageBlob, err error) {
-	var image images.Image
+func (c *Cacheless) GetImage(filename string, width, height uint) (blob images.ImageBlob, err error) {
+	// Search requested image from all roots by trial and error
+	image, err := c.searchRoots(filename)
+	if err != nil {
+		return
+	}
+	// Unlike in GetOriginal Destroy is needed since Image.Resized creates a copy of this original image
+	defer image.Destroy()
+	// Get resized image
+	wx := strconv.FormatUint(uint64(width), 10)
+	wy := strconv.FormatUint(uint64(height), 10)
+	log.Println("Resize to: " + wx + "x" + wy)
+	image, err = image.Resized(width, height)
+	if err != nil {
+		return
+	}
+	// Extract requested format/extension from the filename
+	ext := strings.TrimLeft(filepath.Ext(filename), ".")
+	//Convert the resized image to requested format
+	// TODO: Unsupported format -> Convert does nothing and we get the default format (jpg). Is this behaviour fine?
+	log.Println("Converting: " + ext)
+	err = image.Convert(ext)
+	if err != nil {
+		return
+	}
+	// Get blob from the image. This destroys the image object.
+	blob = image.ToBlob()
+	return
+}
 
-	if len(self.Roots) == 0 {
+
+// Get original image as Image type (for resizing/converting)
+func (c *Cacheless) GetOriginal(filename string) (blob images.ImageBlob, err error) {
+	// Search roots for the Image
+	image, err := c.searchRoots(filename)
+	if err != nil {
+		return
+	}
+	// Extract requested format/extension from the filename
+	ext := strings.TrimLeft(filepath.Ext(filename), ".")
+	//Convert image to requested format
+	// TODO: Unsupported format -> Convert does nothing and we get the default format (jpg). Is this behaviour fine?
+	log.Println("Converting: " + ext)
+	err = image.Convert(ext)
+	if err != nil {
+		return
+	}
+	// Get blob from the Image. This also destroys the image -object.
+	blob = image.ToBlob()
+	return
+}
+
+
+// Search root for an image. Returned image should be destroyed by image.Destroy, image.Resized or image.ToBlob or other.
+func (c *Cacheless) searchRoots(filename string) (image images.Image, err error) {
+	if len(c.Roots) == 0 {
 		err = os.ErrNotExist
 		return
 	}
-
 	// Extract requested type/extension and id from filename
 	ext := strings.TrimLeft(filepath.Ext(filename), ".")
 	id := filename[0:len(filename)-len(ext) - 1]
-	
 	// Search requested image from all roots by trial and error
-	for _, root := range self.Roots {
+	for _, root := range c.Roots {
 		// TODO: Fix escape vulnerability (sanitize filename from at least ".." etc)
-		
 		// Assume image is stored as .jpg -> change extension to .jpg
 		trial := filepath.Join(root, id) + ".jpg"
-
 		image, err = images.LoadImage(trial)
 		if err == nil {
 			log.Println("Found: " + trial)
@@ -44,31 +92,9 @@ func (self *Cacheless) GetImage(filename string, width, height uint) (blob image
 		}
 		log.Println("Not found: " + trial)
 	}
-	if err != nil {
-		return
-	}
-
-	defer image.Destroy()
-
-	wx := strconv.FormatUint(uint64(width), 10)
-	wy := strconv.FormatUint(uint64(height), 10)
-	log.Println("Resize to: " + wx + "x" + wy)
-	image, err = image.Resized(width, height)
-	if err != nil {
-		return
-	}
-	
-	//Convert image to requested format
-	// TODO: Unsupported format -> Convert does nothing and we get the default format (jpg). Is this behaviour fine?
-	log.Println("Converting: " + ext)
-	err = image.Convert(ext)
-	if err != nil {
-		return
-	}
-
-	blob = image.ToBlob()
 	return
 }
+
 
 // A very trivial (and inefficient way to handle roots)
 // Can be used for development work, however.
--- server/cache/imagecache.go
+++ server/cache/imagecache.go
@@ -1,12 +1,12 @@
 package cache
 
 import (
-	"errors"
 	"github.com/phzfi/RIC/server/images"
 )
 
 type ImageCache interface {
 	GetImage(filename string, width, height uint) (images.ImageBlob, error)
+	GetOriginal(filename string) (images.ImageBlob, error)
 
 	// TODO: These could be moved a separate filesystem handler
 	// but these are ok, where they are at the moment (no need to bloat yet).
@@ -21,7 +21,8 @@ type AmbiguousSizeImageCache struct {
 func (a AmbiguousSizeImageCache) GetImage(filename string, width, height *uint) (blob images.ImageBlob, err error) {
 
 	if width == nil || height == nil {
-		return nil, errors.New("Unspecified image size not supported.")
+		blob, err = a.ImageCache.GetOriginal(filename)
+		return
 	}
 
 	blob, err = a.ImageCache.GetImage(filename, *width, *height)
