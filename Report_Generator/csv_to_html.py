e0ffd9be039c7fc680e330cca26a283e7900fd2f
Lauri Telama <telamal1@aalto.fi>
2015-12-22 19:44:02 +0200

    Forgot to add the changed file into last commit

--- Report_Generator/csv_to_html.py
+++ Report_Generator/csv_to_html.py
@@ -6,19 +6,22 @@ Created on 18 Dec 2015
 import csv
 import codecs
 import traceback
+import sys
+
+
+
 
 HIGHLIGHT_HIGHER = (1,3,5,6,7,8)
 HIGHLIGHT_LOWER = (2,4,9)
-GREEN = '''bgColor="rgb(141,245,148)"'''
-YELLOW = '''bgColor="rgb(255,250,205)"'''
 
 def main():
-    ric_file = 'csvFiles/ric.csv'
-    compet_file = 'csvFiles/tumbor.csv'
-    ric_data = read_csv_row(ric_file, 1)
-    compet_data = read_csv_row(compet_file, 1)
-    data = [ric_data, compet_data]
-    titles = read_csv_row(ric_file, 0)
+    args = sys.argv
+    if len(args) < 2:
+        print("U DOIN IT WRONG SON")
+    data = []
+    for csv in args[1:]:
+        data.append(read_csv_row(csv, 1))
+    titles = read_csv_row(args[1], 0)
     html = buildHTML(data, titles)
     save_to_html(html, 'pythonTest.html')
 
@@ -38,20 +41,20 @@ def build_row(row_data, title, i):
     column_html = '<td>%s</td>\n' % title
     if not is_neutral(i) and all(row_data[0] == data for data in row_data):
         for d in row_data:
-            column_html += ('<td %s>%s</td>\n' % (YELLOW, d))
+            column_html += ('<td class="even">%s</td>\n' % d)
     elif not is_neutral(i):
         val = max(row_data)
         if i in HIGHLIGHT_LOWER:
             val = min(row_data)
         for d in row_data:
             if d == val:
-                column_html += ('<td %s>%s</td>\n' % (GREEN, d))
+                column_html += ('<td class="best">%s</td>\n' % d)
             else:
-                column_html += ('<td>%s</td>\n' % d)
+                column_html += ('<td class="neutral">%s</td>\n' % d)
     else:
         for d in row_data:
             column_html += ('<td>%s</td>\n' % d)
-    return ('<tr>%s</td>\n' % column_html) 
+    return ('<tr>%s</tr>\n' % column_html) 
 
             
 
@@ -60,9 +63,9 @@ def build_row(row_data, title, i):
 def buildHTML(data, titles):
     html_table = '''<table>\n
                   <tr>\n
-                  <td></td>\n
-                  <td>RIC</td>\n
-                  <td>Tumbor</td>\n
+                  <th></th>\n
+                  <th>RIC</th>\n
+                  <th>Tumbor</th>\n
                   </tr>\n'''
     for i in range(len(titles)):
         row_data = []
@@ -73,9 +76,13 @@ def buildHTML(data, titles):
     html =   '''<!DOCTYPE html>\n
                 <html>\n
                 <head>\n
+                <link rel="stylesheet" 
+                type="text/css" 
+                href="tablestyle.css">
                 </head>\n
                 <body>\n
                 %s
+                </table>
                 </body>\n
                 </html> ''' % html_table
     return html
@@ -87,12 +94,13 @@ def read_csv_row(from_path, row_number):
             i = 0
             for l in reader:
                 if i == row_number and i != 0:
+                    data = [x.strip() for x in l]
                     data = [l[0],int(l[1]),
                             float(l[2]),int(l[3]),
                             float(l[4]),float(l[5]),
                             float(l[6]),float(l[7]),
                             int(l[8]),int(l[9])]
-                    return data
+                    return data 
                 elif i == row_number:
                     return l
                 i += 1
