d449cf72ed8b2b74b6a46f2633b478d363b53411
Joonatan Saarhelo <joonatan.saarhelo@aalto.fi>
2015-12-27 15:32:35 +0200

    sped up cache tests with mock

--- server/cache/cache_test.go
+++ server/cache/cache_test.go
@@ -1,9 +1,53 @@
 package cache
 
 import (
+	"errors"
+	"github.com/phzfi/RIC/server/images"
 	"path/filepath"
 	"testing"
 )
+
+type DummyResizer bool
+
+func (d DummyResizer) GetImage(fn string, w, h uint) (images.ImageBlob, error) {
+	if d {
+		if fn == "toserve.jpg" {
+			return make(images.ImageBlob, 1001), nil
+		} else {
+			return nil, errors.New("That is not the name of my image!")
+		}
+	} else {
+		return nil, errors.New("RemoveRoot was called last, so I assume I shouldn't find my image.")
+	}
+}
+
+func (d DummyResizer) ImageSize(_ string) (uint, uint, error) {
+	return 1000, 1000, nil
+}
+
+func (d *DummyResizer) AddRoot(_ string) error {
+	*d = true
+	return nil
+}
+
+func (d *DummyResizer) RemoveRoot(_ string) error {
+	*d = false
+	return nil
+}
+
+func makeCacheMaker(policyMaker func() Policy) CacheMaker {
+	return func(mm uint64) *Cache {
+		return NewCache(new(DummyResizer), policyMaker(), mm)
+	}
+}
+
+func TestLRU(t *testing.T) {
+	RunTests(makeCacheMaker(func() Policy { return NewLRUPolicy() }), t)
+}
+
+func TestFIFO(t *testing.T) {
+	RunTests(makeCacheMaker(func() Policy { return new(FIFO) }), t)
+}
 
 type T struct {
 	*testing.T
@@ -21,7 +65,7 @@ func init() {
 	path = filepath.FromSlash("../testimages/cache")
 }
 
-type CacheMaker func(uint64) ImageCache
+type CacheMaker func(uint64) *Cache
 
 func RunTests(newResizer CacheMaker, t_in *testing.T) {
 
@@ -89,7 +133,7 @@ func errorTest(t T, newResizer CacheMaker) {
 
 func cacheExit(t T, newResizer CacheMaker) {
 	// 50kB cache
-	cache := newResizer(50 * 1024)
+	cache := newResizer(4 * 1024)
 
 	t.FatalIfError(cache.AddRoot(path))
 
