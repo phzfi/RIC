a351f369b9a3f2fdbe07edd764e3b17cf82c4167
Lauri Keronen <lauri.keronen@aalto.fi>
2016-01-13 12:47:07 +0200

    Vagrant setup no longer creates two source directories and now creates working go file structure out of the box. Synchronized folder is ~/go/src/github.com/phzfi/RIC/ in virtual machine. Got rid of testresults-folder linking.

--- vagrant_setup.sh
+++ vagrant_setup.sh
@@ -5,13 +5,10 @@ sudo apt-get install -y golang-go dpkg-dev build-essential
 echo "export GOPATH=/home/vagrant/go" >> /home/vagrant/.bashrc
 export GOPATH=/home/vagrant/go
 chown -R vagrant:vagrant /home/vagrant/go
-go get github.com/joonazan/imagick/imagick
-go get gopkg.in/tylerb/graceful.v1
-go install github.com/phzfi/RIC/server
 
-sudo mv /home/vagrant/go/src/github.com/phzfi/RIC/server/testresults/ testresults
-sudo ln -s /home/vagrant/testresults/ /home/vagrant/go/src/github.com/phzfi/RIC/server/testresults
-sudo chown -R vagrant:vagrant /home/vagrant/testresults/
+go get github.com/phzfi/RIC/...
+sudo rm -rf /home/vagrant/go/src/github.com/phzfi
+sudo mv /home/vagrant/go/phzfi /home/vagrant/go/src/github.com
 
 cd /tmp
 mkdir imagemagick
