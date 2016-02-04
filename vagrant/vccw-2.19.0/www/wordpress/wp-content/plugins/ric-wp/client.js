3d2ecab4904f4459a86f39e9c51d6a6edd2bb034
Nicholas Saarela <nicholas.saarela@gmail.com>
2016-02-04 15:34:18 +0200

    added default.yml and updated client.js

--- vagrant/vccw-2.19.0/www/wordpress/wp-content/plugins/ric-wp/client.js
+++ vagrant/vccw-2.19.0/www/wordpress/wp-content/plugins/ric-wp/client.js
@@ -1,18 +1,35 @@
 
 window.onload = function() {
-  setImdivSize();
-  setScreenInfo();
-  formats = [
+  var formats = [
     ['webp', 'webp'],
     ['jpeg', 'jpg'],
     ['png', 'png'],
     ['bmp', 'bmp']
   ];
-  document.getElementById('buttonID').onclick = function() {
-    var id = document.getElementById('imgname').value;
+  fmt = '';
+  for (var i = 0; i < formats.length; i++) {
+    if (supportsIMG(formats[i][0])) {
+      fmt = '.' + formats[i][1];
+      break;
+    }
+  }
+  var elements = document.getElementsByClassName('ricimg');
+  
+  for (var i = 0; i < elements.length; i++){
+    var id = elements[i].id;
+    console.log(id);
+    var h = elements[i].parentElement.clientHeight;
+    var w = elements[i].parentElement.clientWidth;
+    var url = 'http://ric.phz.fi:8005/'+id+fmt+'?width='+w+'&height='+h;
+    elements[i].src = url;
+  }
+
+  //}
+  //document.getElementById('buttonID').onclick = function() {
+    /*var id = document.getElementById('imgname').value;
     var imgW = document.getElementById('imdiv').clientWidth;
     var imgH = document.getElementById('imdiv').clientHeight;
-
+    
     if (id.indexOf('.') === -1) {
       for (var i = 0; i < formats.length; i++) {
         if (supportsIMG(formats[i][0])) {
@@ -21,17 +38,14 @@ window.onload = function() {
         }
       }
     }
-    var url = 'http://ric.phz.fi:8005/'+id+'?width='+imgW+'&height='+imgH;
-    document.getElementById('I').src = url;
-  };
-}
+    */
+    
+    //document.getElementById('I').src = url;
+  //};
 
-window.onresize = function() {
-  setImdivSize();
-  setScreenInfo();
-  document.getElementById('I').src = "";
-}
 
+  //console.log("testing...");
+}
 
 function supportsIMG(img) {
   var canvas = document.createElement('canvas');
@@ -41,22 +55,4 @@ function supportsIMG(img) {
   return uri.match('image/' + img) !== null;
 }
 
-function setImdivSize() {
-  // use aspect ratio of 16:9 to set image height
-  var imgW = document.getElementById('imdiv').clientWidth;
-  var imgH = Math.round(imgW / 16 * 9);
-  document.getElementById('imdiv').style.height = imgH + 'px';
-}
 
-function setScreenInfo() {
-  // show screen information
-  var w = screen.width;
-  var h = screen.height;
-  var dpr = window.devicePixelRatio;
-  var text = 'Your logical screen size is: ' + w + 'x'+ h + ' (' + dpr + ')<br>';
-  text += 'Maximum image size for you: ' + Math.round(w * dpr) + 'x';
-  text += Math.round(h * dpr) + '<br>';
-  text += '<br>' + 'imgdiv clientWidth: ' + document.getElementById('imdiv').clientWidth;
-  text += '<br>' + 'imgdiv clientHeight: ' + document.getElementById('imdiv').clientHeight;
-  document.getElementById('infodiv').innerHTML = text;
-}
