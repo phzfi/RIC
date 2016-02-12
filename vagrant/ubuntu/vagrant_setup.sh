2133439ffa37c1a7b874563b36f70051fd300690
Kristian Mört <kristian.mort@phz.fi>
2016-02-12 10:04:49 +0200

    Initial commit

--- vagrant/ubuntu/vagrant_setup.sh
+++ vagrant/ubuntu/vagrant_setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
-IMAGELIBS=libwebp-dev libtiff5-dev libpng12-dev
-OPENCL=ocl-icd-opencl-dev opencl-headers ocl-icd-libopencl1
+GLIB="libglib2.0-bin libglib2.0-dev"
+IMAGELIBS="libwebp-dev libtiff5-dev libpng12-dev libjpeg-dev liblqr-1-0-dev"
+OPENCL="ocl-icd-opencl-dev opencl-headers ocl-icd-libopencl1"
+DEVEL="automake autoconf gcc git g++ binutils make mercurial tar pkg-config vim wget"
 
 # Install the absolute requirements
 apt-get update
-apt-get install -y ${IMAGELIBS}
+apt-get install -y ${GLIB}
+apt-get install -y ${DEVEL}
 apt-get install -y ${OPENCL}
-apt-get install -y git gcc binutils automake autoconf
-
-#source "vagrant_setup_imagemagick.sh"
-#source "vagrant_setup_go.sh"
+apt-get install -y ${IMAGELIBS}
 
