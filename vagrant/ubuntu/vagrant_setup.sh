3ea8d111875a5b54ea5fdb054e0d2dd070abe963
Kristian Mört <kristian.mort@phz.fi>
2016-02-12 10:04:49 +0200

    Initial commit

--- vagrant/ubuntu/vagrant_setup.sh
+++ vagrant/ubuntu/vagrant_setup.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install -y libmagickwand-dev
-sudo apt-get install -y golang-go dpkg-dev build-essential
+
+# Install the absolute requirements
+apt-get update
+apt-get install -y libmagickwand-dev libwebp-dev libtiff5-dev libpng12-dev
+apt-get install -y dpkg-dev build-essential devscripts
+apt-get install -y golang-go golang-go.tools golang-golang-x-tools-dev
+
 echo "export GOPATH=/home/vagrant/go" >> /home/vagrant/.bashrc
-export GOPATH=/home/vagrant/go
-chown -R vagrant:vagrant /home/vagrant/go
+export GOPATH="/home/vagrant/go"
+mkdir -p "/home/vagrant/go/bin"
+mkdir -p "/home/vagrant/go/pkg"
+mkdir -p "/home/vagrant/go/src"
 
 cd /home/vagrant/go/src/github.com/phzfi/RIC/
 go get -t ./...
 
-cd /tmp
-mkdir imagemagick
-cd imagemagick
-sudo apt-get build-dep imagemagick -y
-sudo apt-get install libwebp-dev devscripts -y
+# Ownership
+chown -R vagrant:vagrant /home/vagrant/go
+
+mkdir -p /tmp/imagemagick
+cd /tmp/imagemagick
+apt-get build-dep imagemagick -y
 apt-get source imagemagick -y
 cd imagemagick-*
 debuild -uc -us
-sudo dpkg -i ../*magick*.deb
+dpkg -i ../*magick*.deb
+
 
