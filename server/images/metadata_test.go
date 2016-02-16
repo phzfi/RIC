b2aa9b3d55a577e03a2e4cedef23b09600706e19
Lauri Keronen <lauri.keronen@aalto.fi>
2016-02-16 11:47:44 +0200

    more tests.

--- server/images/metadata_test.go
+++ server/images/metadata_test.go
@@ -2,7 +2,6 @@ package images
 
 import (
   "testing"
-  "errors"
   "fmt"
 )
 
@@ -20,21 +19,19 @@ func readImageProfiles(img Image, prefix string) (profiles []string) {
   return
 }
 
-func compare(before []string, after []string) (err error) {
+func compare(t *testing.T, before []string, after []string) {
   if len(before) == len(after) {
     for i, _ := range before {
       if before[i] != before[i] {
-        err = errors.New("Image metadata does not match! Different values.")
-        return
+        t.Fatal("Image metadata does not match! Different values.")
       }
     }
   } else {
-    err = errors.New("Image metadata does not match! Different length.")
+    t.Fatal("Image metadata does not match! Different length.")
   }
-  return
 }
 
-func TestPreserveEXIFJpgToTif(t *testing.T) {
+func TestPreserveEXIFJpgResize(t *testing.T) {
   imgBefore := NewImage()
   imgAfter := NewImage()
   defer imgBefore.Destroy()
@@ -42,16 +39,44 @@ func TestPreserveEXIFJpgToTif(t *testing.T) {
 
   imgBefore.FromFile("../testimages/resize/toresize.jpg")
   EXIFbefore := readImageProperties(imgBefore, "exif:*")
-  fmt.Println(EXIFbefore)
 
-  imgBefore.SetImageFormat("TIFF")
+  imgBefore.Resize(100, 100)
   imgAfter.FromBlob(imgBefore.Blob())
-
   EXIFafter := readImageProperties(imgAfter, "exif:*")
-  fmt.Println(EXIFafter)
 
-  err := compare(EXIFbefore, EXIFafter)
-  if err != nil {
-    t.Fatal(err)
-  }
+  compare(t, EXIFbefore, EXIFafter)
+}
+
+func TestPreserveEXIFJpgToPNG(t *testing.T) {
+  imgBefore := NewImage()
+  imgAfter := NewImage()
+  defer imgBefore.Destroy()
+  defer imgAfter.Destroy()
+
+  imgBefore.FromFile("../testimages/resize/toresize.jpg")
+  EXIFbefore := readImageProperties(imgBefore, "exif:*")
+
+  imgBefore.Convert("PNG")
+  imgAfter.FromBlob(imgBefore.Blob())
+  EXIFafter := readImageProperties(imgAfter, "exif:*")
+
+  compare(t, EXIFbefore, EXIFafter)
+}
+
+func TestPreserveEXIFJpgToTiff(t *testing.T) {
+  imgBefore := NewImage()
+  imgAfter := NewImage()
+  defer imgBefore.Destroy()
+  defer imgAfter.Destroy()
+
+  imgBefore.FromFile("../testimages/resize/toresize.jpg")
+  EXIFbefore := readImageProperties(imgBefore, "exif:*")
+  fmt.Println(imgBefore.GetImageProfiles("*"))
+
+  imgBefore.Convert("TIFF")
+  imgAfter.FromBlob(imgBefore.Blob())
+  EXIFafter := readImageProperties(imgAfter, "exif:*")
+  fmt.Println(imgAfter.GetImageProfiles("*"))
+
+  compare(t, EXIFbefore, EXIFafter)
 }
