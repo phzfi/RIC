4724a984d25ae32197273d84cc4c0fdf401f0452
Kristian Mört <kristian.mort@aalto.fi>
2016-02-19 08:24:59 +0200

    OpenCL stuff

--- vagrant/ubuntu/vagrant_setup.sh
+++ vagrant/ubuntu/vagrant_setup.sh
@@ -1,29 +1,14 @@
 #!/bin/bash
 
+IMAGELIBS=libwebp-dev libtiff5-dev libpng12-dev
+OPENCL=ocl-icd-opencl-dev opencl-headers ocl-icd-libopencl1
+
 # Install the absolute requirements
 apt-get update
-apt-get install -y libmagickwand-dev libwebp-dev libtiff5-dev libpng12-dev
-apt-get install -y dpkg-dev build-essential devscripts
-apt-get install -y golang-go golang-go.tools golang-golang-x-tools-dev
+apt-get install -y ${IMAGELIBS}
+apt-get install -y ${OPENCL}
+apt-get install -y git gcc binutils automake autoconf
 
-echo "export GOPATH=/home/vagrant/go" >> /home/vagrant/.bashrc
-export GOPATH="/home/vagrant/go"
-mkdir -p "/home/vagrant/go/bin"
-mkdir -p "/home/vagrant/go/pkg"
-mkdir -p "/home/vagrant/go/src"
-
-cd /home/vagrant/go/src/github.com/phzfi/RIC/
-go get -t ./...
-
-# Ownership
-chown -R vagrant:vagrant /home/vagrant/go
-
-mkdir -p /tmp/imagemagick
-cd /tmp/imagemagick
-apt-get build-dep imagemagick -y
-apt-get source imagemagick -y
-cd imagemagick-*
-debuild -uc -us
-dpkg -i ../*magick*.deb
-
+#source "vagrant_setup_imagemagick.sh"
+#source "vagrant_setup_go.sh"
 
