18b609c7d985be6adcc0687886f1a96a1f737ea8
Kristian Mört <kristian.mort@aalto.fi>
2016-03-02 20:29:22 +0200

    pkg-config + other devel alterations

--- vagrant/ubuntu/vagrant_setup.sh
+++ vagrant/ubuntu/vagrant_setup.sh
@@ -2,10 +2,11 @@
 
 IMAGELIBS=libwebp-dev libtiff5-dev libpng12-dev libjpeg-dev liblqr-1-0-dev
 OPENCL=ocl-icd-opencl-dev opencl-headers ocl-icd-libopencl1
+DEVEL=git-all gcc g++ binutils automake autoconf pkg-config
 
 # Install the absolute requirements
 apt-get update
 apt-get install -y ${IMAGELIBS}
 apt-get install -y ${OPENCL}
-apt-get install -y git gcc binutils automake autoconf
+apt-get install -y ${DEVEL}
 
