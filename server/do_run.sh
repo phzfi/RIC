f65b3c38f842b58b8e9d166e15dfb6aa1f824702
Lauri Telama <telamal1@aalto.fi>
2016-03-04 16:20:00 +0200

    renamed variable in do_run.sh

--- server/do_run.sh
+++ server/do_run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-TEST=`screen -ls | grep RIC | wc -l`
-if [ $TEST = "0" ]; then
-  echo "Zero RIC found"
+COUNT=`screen -ls | grep RIC | wc -l`
+if [ $COUNT = "0" ]; then
+  echo "Zero RIC screens found"
 else
-  echo $TEST " found"
+  echo $COUNT " RIC screens found"
   screen -ls | awk -vFS='\t|[.]' '/RIC/ {system("screen -S "$2" -X quit")}'
 fi
 screen -dmS RIC ./server_loop.sh
