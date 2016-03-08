c7da2ef5fd2ab4e74d64ae32373ec05db20b1422
Joona Haavisto <haavisto.joona@gmail.com>
2016-03-08 17:51:44 +0200

    Changed configuration package name back to config

--- server/config/config.go
+++ server/config/config.go
@@ -1,52 +1,70 @@
-package config
+package configuration
 
 import (
   "bitbucket.org/classroomsystems/ini"
+  "github.com/phzfi/RIC/server/logging"
   "strconv"
   "errors"
 )
 
+type Conf struct{
+  conf ini.Config
+}
+
+func ReadConfig(path string) (config Conf, err error) {
+  conf, err := ini.LoadFile(path)
+  if err != nil {
+    logging.Debug("Error reading config " + err.Error())
+    return
+  }
+  config = Conf {
+    conf: conf,
+  }
+  return
+}
 
 
-var conf, err = ini.LoadFile("config.ini")
-
-func GetString(section, key string) (value string) {
-  value, success := conf.Get(section, key)
+func (conf Conf) GetString(section, key string) (value string, err error) {
+  value, success := conf.conf.Get(section, key)
   if (success) {
     return value
   } else {
-    return ""
+    return "", errors.New("Value not found for "+ key +" in "+ section) 
   }
 }
 
-func GetInt(section, key string) (int, error) {
-  str, success := conf.Get(section, key)
-  if (success) {
+func (conf Conf) GetInt(section, key string) (value int, err error) {
+  str, success := conf.conf.Get(section, key)
+  if success {
     return strconv.Atoi(str)
   } else {
-    return 0, errors.New("Value for " + key + " not found.")
+    return 0, errors.New("Value not found for "+ key +" in "+ section)
   }
 }
 
-func GetFloat64(section, key string) (float64, error) {
-  str, success := conf.Get(section, key)
+func (conf Conf) GetUint64(section, key string) (value uint64, err error) {
+  str, success := conf.conf.Get(section, key)
+  if success {
+    return strconv.ParseUint(str, 10, 64)
+  } else {
+    return 0, errors.New("Value not found for "+ key +" in "+ section)
+  }
+}
+
+func (conf Conf) GetFloat64(section, key string) (value float64, err error) {
+  str, success := conf.conf.Get(section, key)
   if (success) {
     return strconv.ParseFloat(str, 64)
   } else {
-    return 0.0, errors.New("Value for " + key + " not found.")
+    return 0.0, errors.New("Value not found for "+ key +" in "+ section)
   }
 }
 
-func GetBool(section, key string) (bool, error) {
-  str, success := conf.Get(section, key)
+func (conf Conf) GetBool(section, key string) (value bool, err error) {
+  str, success := conf.conf.Get(section, key)
   if (success) {
     return strconv.ParseBool(str)
   } else {
-    return false, errors.New("Value for " + key + " not found.")
+    return false, errors.New("Value not found for "+ key +" in "+ section)
   }
-}
-
-
-func Config() (*ini.Config) {
-  return &conf
 }
