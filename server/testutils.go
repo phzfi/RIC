f232072b63f244c8dbd2be943cb2fb2e7303c3d9
Miika Piiroinen <miika.piiroinen@aalto.fi>
2016-03-10 18:41:44 +0200

    Fixed liquid test tolerances

--- server/testutils.go
+++ server/testutils.go
@@ -60,12 +60,10 @@ func getBlobFromServer(getname string) (blob images.ImageBlob, err error) {
 
 // Tests getting images. c.Testfn is treated as GET string (filename?params).
 // Executes the tests supported by TestCaseAll.
-func testGetImages(cases []images.TestCaseAll) (err error) {
+func testGetImages(cases []images.TestCaseAll, tolerance float64) (err error) {
 
 	s, ln, srverr := startServer()
 	defer stopServer(s, ln, srverr)
-
-	tolerance := 0.002
 
 	// Todo: this threading is copied from common_test.go unify it to single implementation (DRY)
 	sem := make(chan error, len(cases))
