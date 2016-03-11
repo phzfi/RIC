81166464f3ad9c3d5e02eba6b81ce7bff83a274e
Lauri Keronen <lauri.keronen@aalto.fi>
2016-03-11 13:42:48 +0200

    The script now supports all image server features and takes possible computer browser zoom level into account.

--- vagrant/vccw-2.19.0/www/wordpress/wp-content/plugins/ric-wp/client.js
+++ vagrant/vccw-2.19.0/www/wordpress/wp-content/plugins/ric-wp/client.js
@@ -19,49 +19,62 @@ window.onload = function() {
 
   // loop over all elements of page and inject to image divs RIC URL's
   for (var i = 0; i < elements.length; i++){
-    var id = elements[i].id;
+    var id = elements[i].dataset.id;
     // if no explicit format has been given in the id, use the most appropriate
     if(id.indexOf('.') == -1){
       id += fmt;
     }
 	
-	// read width and height from parent element dimensions
-	// and multiply them with device pixel ratio for high dpi screens
-	var dpr = window.devicePixelRatio;
-	var width = 'width=' + elements[i].parentElement.clientWidth * dpr;
-	var height = 'height=' + elements[i].parentElement.clientHeight * dpr;
+	// read width and height from parent element
+	var width = elements[i].parentElement.clientWidth;
+	var height = elements[i].parentElement.clientHeight;
 	
-	// if no height specified (in HTML!), request image using width only
-	if (elements[i].parentElement.outerHTML.indexOf('height') == -1) {
-		var img = new Image();
-		img.src = URI + id + '?' + width;
-		elements[i].parentElement.clientHeight = img.height;
-		elements[i].src = img.src;
-	} else {
-		var params = [width, height];
-		
-		// set image resize mode parameter - fit to parent dimensions as default
-		var mode;
-		if (elements[i].classList.contains('mode-nofit')) {}
-		else if (elements[i].classList.contains('mode-liquid')) {
-			mode = 'mode=liquid';
-			params.push(mode);
-		}
-		else {
-			mode = 'mode=fit';
-			params.push(mode);
-		}
-		
-		// extend uri with the parameters and set image source
-		var url = URI + id;
-		if (params.length > 0) {
-			url += '?' + params[0];
-			for (var j = 1; j < params.length; j++) {
-				url += '&' + params[j];
-			}
-		}
-		elements[i].src = url;
+	// Use zoom coefficient for large screens
+	if (screen.width > 1000) {
+		width = Math.round(width * window.devicePixelRatio);
+		height = Math.round(height * window.devicePixelRatio);
 	}
+	
+	var url = URI + id;
+	var params = ['width=' + width];
+	if (elements[i].parentElement.outerHTML.indexOf('height') != -1)
+		params.push('height=' + height);
+	
+	// set image resize mode parameter - fit to parent dimensions as default
+	if (elements[i].dataset.mode == 'nofit') {}
+	else if (elements[i].dataset.mode == 'liquid') {
+		params.push('mode=liquid');
+	}
+	else {
+		params.push('mode=fit');
+	}
+	
+	// extend uri with the parameters and set image source
+	var url = URI + id;
+	if (params.length > 0) {
+		url += '?' + params[0];
+		for (var j = 1; j < params.length; j++) {
+			url += '&' + params[j];
+		}
+	}
+	elements[i].src = url;
+	elements[i].onload = function() {	// Set maximum image css resolution
+		if (screen.width > 1000) {
+			this.width = Math.round(this.naturalWidth / window.devicePixelRatio);
+			this.height = Math.round(this.naturalHeight / window.devicePixelRatio);
+		} else {
+			this.width = this.naturalWidth;
+			this.height = this.naturalHeight;
+		}
+		
+		// if original image is returned, fit to content
+		if (this.height > this.parentElement.clientHeight) {
+			var dh = this.height - this.parentElement.clientHeight;
+			var ratio = this.width / this.height;
+			this.width = Math.round(this.width - dh * ratio);
+			this.height = this.parentElement.clientHeight;
+		}
+	};
   }
 }
 
