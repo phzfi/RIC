28c390ff9551d311ed369e5a79e7962b70489357
Lauri Telama <lauri.telama@aalto.fi>
2016-03-19 17:02:44 +0200

    Added test for ric constant requests

--- report_generator/ric_tests/run_siege_ric_constant_requests_local.sh
+++ report_generator/ric_tests/run_siege_ric_constant_requests_local.sh
@@ -1,18 +1,49 @@
 #!/bin/bash
 
-URLS_FILE=./urls.txt
-RAW_FILE=./raw/ric_$(date +%Y-%m-%d_%H-%M-%S).txt
-OUT_FILE=./results/ric_$(date +%Y-%m-%d_%H-%M-%S).csv
-TMP=./temp/$(date +%Y-%m-%d_%H-%M-%S).tmp
+
+if [ $# -lt 3 ]; then
+echo "Script usage: sh run_siege_same_request_amount.sh RANDOM_SEED CONCURRENT_USERS REQUESTS_PER_USER"
+exit 1
+fi
+
+# Checks if user started program properly, if not then starts it
+if [ -z $BASH_VERSION ]; then
+	exec bash "$0" "$@"
+	exit $?
+fi
+
+# cd to report_generator
+DIR="$( cd "$(dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd "$DIR"
+cd ..
+
+# URLS randomiser
+URLS_FILE=siege_url_files/urls_local.txt
+SEED=$1
+python3 siege_url_files/urls_randomizer.py "$URLS_FILE" $SEED
+URLS_FILE="${URLS_FILE%.*}"_temp.txt
+
+# Siege settings
+DELAY=1
 SIEGE_CONF=./.siegerc
-CONCURRENT=50
-TIME="300s"
+CONCURRENT=$2
+REQUESTS_PER_USER=$3
+
+
+#RIC SIEGE
+RAW_FILE=./raw/ric_$(date +%Y-%m-%d_%H-%M-%S).txt
+RIC_OUT_FILE=./results/ric_$(date +%Y-%m-%d_%H-%M-%S).csv
+TMP=./temp/$(date +%Y-%m-%d_%H-%M-%S).tmp
 
 # Siege
-siege -R $SIEGE_CONF --verbose --concurrent=$CONCURRENT -b --time=$TIME --log=$RAW_FILE --file=$URLS_FILE |
+siege -R $SIEGE_CONF --verbose --concurrent=$CONCURRENT --delay=$DELAY -r$REQUESTS_PER_USER --log=$RAW_FILE --file=$URLS_FILE |
 	 sed -r "s/\x1B\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]//g" > $TMP
 cat $TMP >> $RAW_FILE
 rm $TMP
 
+python3 csv_formatter.py $RAW_FILE $RIC_OUT_FILE
+
 # Formatter
-python csv_formatter.py $RAW_FILE $OUT_FILE
+rm $URLS_FILE
+
+python3 csv_to_html.py html_tables/ricConstantRequestsResultsLocal.html $RIC_OUT_FILE
