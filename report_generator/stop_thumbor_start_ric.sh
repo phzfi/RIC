54acf4d00e0e97cfe08365e453ae75ec62ccee40
Lauri Telama <lauri.telama@aalto.fi>
2016-03-19 19:23:44 +0200

    Added thumbor and its scripts so that it is easier to be run

--- report_generator/stop_thumbor_start_ric.sh
+++ report_generator/stop_thumbor_start_ric.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 ssh phzfi@ric.phz.fi << EOF
-        cd;
-        cd go/src/github.com/phzfi/RIC/scripts;
-        sh stop_thumbor_start_ric.sh;
+  cd;
+  cd go/src/github.com/phzfi/RIC/scripts;
+  sh stop_thumbor_start_ric.sh;
 EOF
---
+++ scripts/start_cib_stop_rest.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#shutdown RIC, CIB and Thumbor Instances
+screen -ls | awk -vFS='\t|[.]' '/RIC/ {system("screen -S "$2" -X quit")}'
+screen -ls | awk -vFS='\t|[.]' '/CIB/ {system("screen -S "$2" -X quit")}'
+screen -ls | awk -vFS='\t|[.]' '/Thumbor/ {system("screen -S "$2" -X quit")}'
+
+cd ~/go/src/github.com/phzfi/RIC/competing_product/
+
+sh cib_do_run.sh
