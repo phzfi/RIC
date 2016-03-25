09575c7e40495cee5a76cde0791f8bed26510e7d
Miika Piiroinen <miika.piiroinen@aalto.fi>
2016-03-25 13:59:36 +0200

    Added debug prints to PingRoots test

--- server/ops/ping_image_test.go
+++ server/ops/ping_image_test.go
@@ -4,11 +4,13 @@ import (
 	"testing"
 	"time"
 	"github.com/phzfi/RIC/server/images"
+	"github.com/phzfi/RIC/server/logging"
 	"fmt"
 )
 
 
 func TestPingRoots(t *testing.T) {
+	logging.Debugf("Testing PingRoots...")
 
 	is := MakeImageSource()
 	err := is.AddRoot("../")
@@ -42,7 +44,8 @@ func TestPingRoots(t *testing.T) {
 		t.Fatal(err)
 	}
 	t3 := time.Now().Sub(t0)
-
+	
+	logging.Debugf("searchRoots #1: %v, pingRoots: %v, searchRoots #2: %v", t1, t2, t3)
 	if t1 < t2 {
 		t.Fatal(fmt.Sprintf("pingRoots is slower than searchRoots! pingRoots: %v, searchRoots: %v", t2, t1))
 	}
