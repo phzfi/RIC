62f380c202da3adf7b0139af0e2d075ab1e5e5d3
Lauri Keronen <lauri.keronen@aalto.fi>
2016-03-25 13:13:01 +0200

    Use larger image to ensure that PingImage is faster than FromFile.

--- server/ops/ping_image_test.go
+++ server/ops/ping_image_test.go
@@ -19,34 +19,34 @@ func TestPingRoots(t *testing.T) {
 	img := images.NewImage()
 	defer img.Destroy()
 	t0 := time.Now()
-	err = is.searchRoots("testimages/loadimage/test.jpg", img)
+	err = is.searchRoots("testimages/loadimage/22.jpg", img)
 	if err != nil {
 		t.Fatal(err)
 	}
 	t1 := time.Now().Sub(t0)
-	
+
 	img = images.NewImage()
 	defer img.Destroy()
 	t0 = time.Now()
-	err = is.pingRoots("testimages/loadimage/test.jpg", img)
+	err = is.pingRoots("testimages/loadimage/22.jpg", img)
 	if err != nil {
 		t.Fatal(err)
 	}
 	t2 := time.Now().Sub(t0)
-	
+
 	img = images.NewImage()
 	defer img.Destroy()
 	t0 = time.Now()
-	err = is.searchRoots("testimages/loadimage/test.jpg", img)
+	err = is.searchRoots("testimages/loadimage/22.jpg", img)
 	if err != nil {
 		t.Fatal(err)
 	}
 	t3 := time.Now().Sub(t0)
-	
+
 	if t1 < t2 {
 		t.Fatal(fmt.Sprintf("pingRoots is slower than searchRoots! pingRoots: %v, searchRoots: %v", t2, t1))
 	}
-	
+
 	if t3 < t2 {
 		t.Fatal(fmt.Sprintf("pingRoots is slower than searchRoots! pingRoots: %v, searchRoots: %v", t2, t3))
 	}
