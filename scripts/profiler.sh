d89a8f69736ce2abdc6f5c0ec76427c2836ab3de
Lauri Telama <lauri.telama@aalto.fi>
2016-03-29 19:37:53 +0300

    Updated profiler and cib installation scripts

--- scripts/profiler.sh
+++ scripts/profiler.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 screen -ls | awk -vFS='\t|[.]' '/RIC/ {system("screen -S "$2" -X quit")}'
-cd $GOPATH/src/github.com/phzfi/RIC/server
+export GOPATH=/home/vagrant/go
+sudo sh ~/go/src/github.com/phzfi/RIC/scripts/clear_cache.sh
+cd ~/go/src/github.com/phzfi/RIC/server
+echo "Building server"
 go build -tags profile && sh do_run.sh
 echo "Waiting for ric to start"
 sleep 4s
 cd ../report_generator
-sh siege_test.sh -l -d 0 -t 40 -c 200 & go tool pprof -svg http://localhost:6060/debug/pprof/profile > ../scripts/profiler_results/$(date +%Y-%m-%d_%H-%M-%S).svg
+DATE=$(date +%Y-%m-%d_%H-%M-%S)
+sh siege_test.sh -l -d 0 -t 35 -c 40 & go tool pprof -svg http://localhost:6060/debug/pprof/profile > ../scripts/profiler_results/fresh_$DATE.svg
+sleep 30s
+echo "Starting again"
+sh siege_test.sh -l -d 0 -t 35 -c 40 & go tool pprof -svg http://localhost:6060/debug/pprof/profile > ../scripts/profiler_results/cached_$DATE.svg
