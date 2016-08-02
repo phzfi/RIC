6dac68dc2014b5b6088de691fd5b4598643fb1aa
phz <phz@phz.fi>
2016-08-02 14:25:38 +0300

    make checking whether a root exists a function

--- server/ops/roots.go
+++ server/ops/roots.go
@@ -12,16 +12,23 @@ var (
 
 type roots []string
 
-func (roots *roots) Add(n string) error {
-	logging.Debug("Adding root: " + n)
-	for _, path := range *roots {
-		if path == n {
-			return ErrRootAlreadyAdded
-		}
+func (roots *roots) Add(r string) error {
+	logging.Debug("Adding root: " + r)
+	if roots.HasRoot(r) {
+		return ErrRootAlreadyAdded
 	}
 
-	*roots = append(*roots, n)
+	*roots = append(*roots, r)
 	return nil
+}
+
+func (roots *roots) HasRoot(r string) bool {
+	for _, path := range *roots {
+		if path == r {
+			return true
+		}
+	}
+	return false
 }
 
 func (roots *roots) Remove(r string) error {
