066da6c8567189629bcdaf55937d9c5e89465536
Your Name <you@example.com>
2019-01-28 09:22:54 +0200

    Added path export

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 export GOPATH=/root/go
+export PATH=$PATH:$GOPATH/bin
+
+cd "$GOPATH/src/github.com/phzfi/RIC/server"
 
 go get -u github.com/jstemmer/go-junit-report
 go get github.com/t-yuki/gocover-cobertura
 
-
-cd "$GOPATH/src/github.com/phzfi/RIC/server"
 
 go test -v -coverprofile=cover.out ./ 2>&1 |  go-junit-report > junit.xml
 
