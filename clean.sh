bbb3c1a45828873d130a4ce217cf3325b9ce3855
Sami Kaskinen <sami@phz.fi>
2023-10-02 16:44:50 +0300

    Fix clean.sh path

--- clean.sh
+++ clean.sh
@@ -2,7 +2,7 @@
 #Clean test results etc
 
 #Vagrant
-~/workspace/vagrant/phz-vagrant-metadata/purge-boxes.sh
+~/workspace/vagrant/phz-vagrant-metadata/scripts/purge-boxes.sh
 
 #Clean up sh2ju test results before next build to avoid cumulation
 rm -fr results/*.xml
