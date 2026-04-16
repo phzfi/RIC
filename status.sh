2db55b6c1d1f5187debb9a4419674a437cd77869
Antti Hätinen <pharazon@phz.fi>
2026-04-16 08:05:04 +0300

    Updated status.sh to use docker compose

--- status.sh
+++ status.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-vagrant status
+SERVICE=ric
+if test "$1" == 'stg' || test "$1" == 'prod'; then
+    export DOCKER_HOST=docker-swarm-master.in.phz.fi
+    docker stack ps $SERVICE-$1
+else
+    export DOCKER_HOST=
+    docker compose ps -a
+fi
