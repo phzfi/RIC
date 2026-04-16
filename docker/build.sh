97ebc446a4a9639c85a68d024de2d3ea23293cf3
Antti Hätinen <pharazon@phz.fi>
2026-04-16 08:00:59 +0300

    Use build-env in tag

--- docker/build.sh
+++ docker/build.sh
@@ -6,7 +6,7 @@ NAME=phzfi/ric
 #No need to change anything below this line
 BUILD_ENV=$1
 VERSION=$2
-TAG=$NAME:$VERSION
+TAG=$NAME:$BUILD_ENV-$VERSION
 
 if ! ( [ "$BUILD_ENV" == "dev" ] || [ "$BUILD_ENV" == "stg" ] || [ "$BUILD_ENV" == "prod" ] ) || [ -z "$VERSION" ]; then
     echo "Usage: ./build.sh [dev|stg|prod] <version>, e.g. ./build.sh stg stg-123"
