8163c534f709a9aeb81262545834860a2f1ec376
Antti Hätinen <pharazon@phz.fi>
2026-04-19 20:57:59 +0300

    Just run test container to generate coverage

--- coverage.sh
+++ coverage.sh
@@ -6,6 +6,6 @@ cd "$(dirname "$0")"
 workspace="."
 mkdir -p ${workspace}/reports/coverage
 
-docker compose run --rm test sh -c 'go test -v -coverprofile=reports/coverage/coverage.out ./server/... && go tool cover -html=reports/coverage/coverage.out -o=reports/coverage/coverage.html'
+docker compose run --rm test
 
 sudo chown -R $(whoami):$(id -gn) ${workspace}/reports/coverage || true
